c6e00f0b8582942f792a82b7022dd91e3db06492
Marcio Silveira <mhsilveira.br@gmail.com>
2020-08-19 14:27:18 -0300

    continued creating graphs for the climatological study

--- climatology.py
+++ climatology.py
@@ -1,8 +1,6 @@
 import os
 import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
-import matplotlib.patches as patches
 import seaborn as sns
 from pathlib import Path
 import warnings
@@ -10,50 +8,44 @@ import warnings
 warnings.filterwarnings('ignore')
 
 
-class Climatology:
+def fix_wx_names(data):
+    # Wx
+    # A csv file with all phenomenon codes was created using the ISD manual
+    # Then they were put in a dict and then replaced in the rows
+    codes = pd.read_csv('./data/input/wx_codes.csv', sep=';', index_col=False)
+    codes_dict = codes['Phenomenon'].to_dict()
+    phenomena = data.filter(like='phenomenon').fillna(0)
+    data[phenomena.columns] = phenomena.replace(codes_dict)
+    return data
 
-    def __init__(self):
-        self.wx_codes_path = './data/input/wx_codes.csv'
 
-    def fix_wx_names(self, data):
-        # Wx
-        # A csv file with all phenomenon codes was created using the ISD manual
-        # Then they were put in a dict and then replaced in the rows
-        codes = pd.read_csv(self.wx_codes_path, sep=';', index_col=False)
-        codes_dict = codes['Phenomenon'].to_dict()
-        phenomena = data.filter(like='phenomenon').fillna(0)
-        data[phenomena.columns] = phenomena.replace(codes_dict)
-        return data
+def plot_wx(data, office, airport):
+    data = fix_wx_names(data)
+    data.index = pd.to_datetime(data['DATE'])
+    data['month'] = data.index.month
+    data['hour'] = data.index.hour
+    phenomena = sorted(set(data['phenomenon_1']))
+    output_path = f'./data/output/{office}/{airport}'
+    Path(output_path).mkdir(parents=True, exist_ok=True)
+    for wx in phenomena:
+        filename = f'{output_path}/wx_{wx}_{airport}_2011-2019.png'
+        if not os.path.exists(filename):
+            filtered_data = data[data['phenomenon_1'] == wx]
+            fig, ax = plt.subplots(figsize=(10, 8))
+            heatmap_data = pd.pivot_table(filtered_data,
+                                          index='hour',
+                                          columns='month',
+                                          values='phenomenon_1',
+                                          fill_value=0,
+                                          aggfunc='count').reindex(sorted(data['hour'].unique()),
+                                                                   columns=data['month'].unique(), fill_value=0)
+            sns.heatmap(heatmap_data, cmap='Blues')
+            xlabels = ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez']
+            ax.set_xticklabels(xlabels)
+            plt.yticks(rotation=0)
+            ax.set_xlabel('Mês')
+            ax.set_ylabel('Hora (UTC)')
+            plt.suptitle(f'Frequência de {wx} em {airport} com dados de 2011 a 2019')
+            plt.savefig(filename)
+            return fig
 
-    def plot_phenomena(self, fixed_data, palette=None):
-        # Filter CAVOK data
-        cavok = fixed_data['phenomenon_1'].isin(['CAVOK'])
-        cavok_data = fixed_data['phenomenon_1'].loc[cavok]
-        non_cavok_data = fixed_data.loc[~cavok]
-        # Plot countplot
-        if palette is None:
-            palette = sns.color_palette('coolwarm', n_colors=non_cavok_data.shape[1])
-        total = float(len(fixed_data))
-        total_cavok = float(len(cavok_data))
-        fig, ax = plt.subplots(figsize=(8, 6))
-        fig = sns.countplot(data=non_cavok_data,
-                            x='phenomenon_1',
-                            order=non_cavok_data['phenomenon_1'].value_counts().index,
-                            color=palette[0])
-        ax.set(xlabel='Fenômeno', ylabel='Número de ocorrências')
-        # Plotting the CAVOk information in the top right position
-        position = fig.patches
-        x_position = position[-1].get_x()
-        y_position = position[0].get_height()
-        ax.text(x_position, y_position,
-                f'Sem fenômeno: {(total_cavok / total) * 100:.2f}%',
-                horizontalalignment='right',
-                verticalalignment='top')
-        # Plotting percentages on the bars
-        for p in fig.patches:
-            height = p.get_height()
-            ax.text(p.get_x() + p.get_width() / 2.,
-                    height + 0.3,
-                    f'{(height / total) * 100:.2f}%',
-                    ha="center")
-        return fig
--- plot.py
+++ plot.py
@@ -3,7 +3,7 @@ from pathlib import Path
 import pandas as pd
 import matplotlib.pyplot as plt
 from my_windrose import WindRose
-from climatology import Climatology
+from climatology import plot_wx
 import warnings
 
 warnings.filterwarnings('ignore')
@@ -21,86 +21,53 @@ for office in offices:
 
     for airport, data in base_data.items():
         # Plot windrose
-        windrose = WindRose()
-        windrose_data = windrose.create_rosedata(data)
-        windrose.create_windrose(windrose_data)
         output_path = f'./data/output/{office}/{airport}'
         Path(output_path).mkdir(parents=True, exist_ok=True)
-        plt.suptitle(f'Rosa dos ventos de {airport} com dados de 2011 a 2019')
         filename = f'{output_path}/windrose_00_{airport}_2011-2019.png'
         if not os.path.exists(filename):
+            windrose = WindRose()
+            windrose_data = windrose.create_rosedata(data)
+            windrose.create_windrose(windrose_data)
+            plt.suptitle(f'Rosa dos ventos de {airport} com dados de 2011 a 2019')
             plt.savefig(filename)
         # Plot wx
-        wx = Climatology()
-        fixed_data = wx.fix_wx_names(data)
-        wx.plot_phenomena(fixed_data)
-        output_path = f'./data/output/{office}/{airport}'
-        Path(output_path).mkdir(parents=True, exist_ok=True)
-        plt.suptitle(f'Frequência de fenômenos significativos em {airport} com dados de 2011 a 2019')
-        filename = f'{output_path}/wx_00_{airport}_2011-2019.png'
-        if not os.path.exists(filename):
-            plt.savefig(filename)
+        plot_wx(data, office, airport)
 
-    # Create the phenomena frequency for each month
-    year = {1: 'Janeiro', 2: 'Fevereiro', 3: 'Março', 4: 'Abril', 5: 'Maio', 6: 'Junho',
-            7: 'Julho', 8: 'Agosto', 9: 'Setembro', 10: 'Outubro', 11: 'Novembro', 12: 'Dezembro'}
+        # Create the phenomena frequency for each month
+        year = {1: 'Janeiro', 2: 'Fevereiro', 3: 'Março', 4: 'Abril', 5: 'Maio', 6: 'Junho',
+                7: 'Julho', 8: 'Agosto', 9: 'Setembro', 10: 'Outubro', 11: 'Novembro', 12: 'Dezembro'}
 
-    for month_number, month_name in year.items():
-        for airport, data in base_data.items():
+        for month_number, month_name in year.items():
             data.index = pd.to_datetime(data['DATE'])
             data = data[data.index.month.isin([month_number])]
             try:
                 # Plot Windrose
-                windrose = WindRose()
-                windrose_data = windrose.create_rosedata(data)
-                windrose.create_windrose(windrose_data)
                 output_path = f'./data/output/{office}/{airport}'
                 Path(output_path).mkdir(parents=True, exist_ok=True)
-                plt.suptitle(f'Rosa dos ventos de {airport} com dados de 2011 a 2019\n'
-                             f'{month_name.upper()}')
                 filename = f'{output_path}/windrose_{month_number:02}_{month_name}_{airport}_2011-2019.png'
                 if not os.path.exists(filename):
-                    plt.savefig(filename)
-                # Plot wx
-                wx = Climatology()
-                fixed_data = wx.fix_wx_names(data)
-                wx.plot_phenomena(fixed_data)
-                output_path = f'./data/output/{office}/{airport}'
-                Path(output_path).mkdir(parents=True, exist_ok=True)
-                plt.suptitle(
-                    f'Frequência de fenômenos significativos em {airport} com dados de 2011 a 2019\n'
-                    f'{month_name.upper()}')
-                filename = f'{output_path}/wx_{month_number:02}_{month_name}_{airport}_2011-2019.png'
-                if not os.path.exists(filename):
+                    windrose = WindRose()
+                    windrose_data = windrose.create_rosedata(data)
+                    windrose.create_windrose(windrose_data)
+                    plt.suptitle(f'Rosa dos ventos de {airport} com dados de 2011 a 2019\n'
+                                 f'{month_name.upper()}')
                     plt.savefig(filename)
             except (ValueError, ZeroDivisionError):
                 continue
 
-    for hour in range(1, 24, 1):
-        for airport, data in base_data.items():
+        for hour in range(0, 24, 1):
             data.index = pd.to_datetime(data['DATE'])
             data = data[data.index.hour.isin([hour])]
             try:
-                windrose = WindRose()
-                windrose_data = windrose.create_rosedata(data)
-                windrose.create_windrose(windrose_data)
                 output_path = f'./data/output/{office}/{airport}'
                 Path(output_path).mkdir(parents=True, exist_ok=True)
-                plt.suptitle(f'Rosa dos ventos de {airport} com dados de 2011 a 2019'
-                             f'\n{hour:02}00 UTC')
                 filename = f'{output_path}/windrose_{hour:02}00UTC_{airport}_2011-2019.png'
                 if not os.path.exists(filename):
-                    plt.savefig(filename)
-                # Plot wx
-                wx = Climatology()
-                fixed_data = wx.fix_wx_names(data)
-                wx.plot_phenomena(fixed_data)
-                output_path = f'./data/output/{office}/{airport}'
-                Path(output_path).mkdir(parents=True, exist_ok=True)
-                plt.suptitle(f'Frequência de fenômenos significativos em {airport} com dados de 2011 a 2019'
-                             f'\n{hour:02}00 UTC')
-                filename = f'{output_path}/wx_{hour:02}00UTC_{airport}_2011-2019.png'
-                if not os.path.exists(filename):
+                    windrose = WindRose()
+                    windrose_data = windrose.create_rosedata(data)
+                    windrose.create_windrose(windrose_data)
+                    plt.suptitle(f'Rosa dos ventos de {airport} com dados de 2011 a 2019'
+                                 f'\n{hour:02}00 UTC')
                     plt.savefig(filename)
             except (ValueError, ZeroDivisionError):
                 continue
