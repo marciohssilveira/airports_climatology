67740194e17f823d0a59115b0f1d14a9c4634b53
Marcio Silveira <marciohssilveira@gmail.com>
2020-12-11 14:31:25 -0300

    fixed windroses

---
+++ src/run.py
@@ -0,0 +1,24 @@
+import os
+import sys
+src_dir = os.path.join(os.getcwd(), '..', 'src')
+sys.path.append(src_dir)
+
+from d01_data.get_data import GetIsdData
+from d03_visualisation.plot_climatology import Climatology
+import pandas as pd
+
+sbgr = GetIsdData('SBGR')
+data = sbgr.download_isd_data()
+
+climatology = Climatology(data, 'SBGR')
+
+climatology.plot_variables_climatology()
+# Plot wx
+climatology.plot_wx()
+# Plot all time windroses
+climatology.plot_windrose()
+# Plot monthly windroses
+climatology.plot_monthly_windrose()
+# Plot hourly windroses
+climatology.plot_hourly_windrose()
+print('Done!')
